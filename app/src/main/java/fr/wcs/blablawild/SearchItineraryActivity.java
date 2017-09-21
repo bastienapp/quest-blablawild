51398693eaa48186e124a673dcc8fce6df0d51cf
bastienwcs <bastien@wildcodeschool.fr>
2017-09-21 13:23:36 +0200

    Quête DatePicker

--- app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/fr/wcs/blablawild/SearchItineraryActivity.java
@@ -1,12 +1,18 @@
 package fr.wcs.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SearchItineraryActivity extends AppCompatActivity {
 
@@ -15,9 +21,9 @@ public class SearchItineraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_itinerary);
 
-        final TextView editDeparture = (TextView) findViewById(R.id.edit_departure);
-        final TextView editDestination = (TextView) findViewById(R.id.edit_destination);
-        final TextView editDate = (TextView) findViewById(R.id.edit_date);
+        final EditText editDeparture = (EditText) findViewById(R.id.edit_departure);
+        final EditText editDestination = (EditText) findViewById(R.id.edit_destination);
+        final EditText editDate = (EditText) findViewById(R.id.edit_date);
         Button buttonSearch = (Button) findViewById(R.id.form_search);
 
         buttonSearch.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +48,36 @@ public class SearchItineraryActivity extends AppCompatActivity {
                 }
             }
         });
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(editDate, myCalendar);
+            }
+        };
+
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(SearchItineraryActivity.this,
+                        dateListener,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+    }
+
+    private void updateLabel(EditText editText, Calendar myCalendar) {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+        editText.setText(sdf.format(myCalendar.getTime()));
     }
 }
